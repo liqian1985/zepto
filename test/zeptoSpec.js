051421966ca73bb7c0b6e0e0324b4cb734808355
Li Qian <li_qian008@163.com>
2015-06-05 09:35:02 +0800

    添加$方法，并验证$方法返回值中的dom,html,css 3个方法

---
+++ test/zeptoSpec.js
@@ -0,0 +1,47 @@
+describe('zepto suite text', function () {
+    var div;
+
+    beforeEach(function () {
+        div = document.createElement('div');
+        div.innerHTML = "<p>text</p><p>text</p><div>innerdiv</div>";
+        document.body.appendChild(div);
+    });
+
+    afterEach(function () {
+        document.body.removeChild(div);
+    });
+
+    describe('$ suite text', function () {
+        it ('验证$函数返回值中包含dom数组属性', function () {
+            var result = $('div');
+
+            expect(result.dom).toEqual(jasmine.any(Array));
+            expect(result.dom.length).toBe(2);
+            expect(result.dom[0]).toBe(div);
+            expect(result.dom[1].innerHTML).toBe('innerdiv');
+        })
+    });
+
+    describe('$.html suite text', function () {
+        it ('验证$函数返回值中包含html方法', function () {
+            var result = $('div');
+            result.html('123');
+            expect(result.html).toEqual(jasmine.any(Function));
+            expect(result.dom[0].innerHTML).toBe('123');
+            expect(result.dom[1].innerHTML).toBe('123');
+        });
+    });
+
+    describe('$.css suite text', function () {
+        it ('验证$函数返回值中包含css方法', function () {
+            var result = $('div');
+            result.css('width:100px');
+            result.css('font-size:12px');
+            expect(result.css).toEqual(jasmine.any(Function));
+            //expect(result.dom[0].style.width).toBe('100px');
+            expect(result.dom[0].style.fontSize).toBe('12px');
+            expect(result.dom[1].style.fontSize).toBe('12px');
+        });
+    });
+
+});
