fb41c73082986127b7fd470028a457ec9b69b06f
Li Qian <li_qian008@163.com>
2015-06-15 10:31:36 +0800

    slice改成局部变量的形式，append,prepend,before,after 这几个方法的整理

--- src/zepto.js
+++ src/zepto.js
@@ -1,10 +1,17 @@
-(function (slice) {
+var $ = (function () {
+    var slice = [].slice,
+        k,
+        ADJ_OPS = {
+            append: 'beforeEnd',
+            prepend: 'afterBegin',
+            before: 'beforeBegin',
+            after: 'afterEnd'
+        };
     function $(_) {
         if (typeof _ == 'function') {
             $.dom.forEach(_);
         } else {
-            $._ = _;
-            $.dom = slice.call(document.querySelectorAll(_));
+            $.dom = slice.call(document.querySelectorAll($._ = _));
         }
         return $.fn;
     }
@@ -16,26 +23,19 @@
                 return $.dom[idx];
             }
         },
+
         html: function (html) {
             return $(function (el) {
                 el.innerHTML = html;
             });
         },
-        append: function (html) {
-            return $(function (el) {
-                el.insertAdjacentHTML('beforeEnd', html);
-            });
-        },
-        prepend: function (html) {
-            return $(function (el) {
-                el.insertAdjacentHTML('afterBegin', html);
-            });
-        },
+
         css: function (style) {
             return $(function (el) {
                 el.style.cssText += ';' + style;
             });
         },
+
         anim: function (transform, opacity, dur) {
             var opa;
             if (opacity === 0) {
@@ -47,6 +47,7 @@
                 '-webkit-transform:' + transform +
                 ';opcity' + opa);
         },
+
         live: function (event, callback) {
             var selector = $._;
             document.body.addEventListener(event, function (event) {
@@ -62,6 +63,18 @@
             return $.fn;
         }
     };
+
+    for (k in ADJ_OPS) {
+        $.fn[k] = (function(op){
+            return function(html) {
+                return $(function (el) {
+                    console.log(op);
+                    el.insertAdjacentHTML(op,html);
+                });
+            };
+        })(ADJ_OPS[k]);
+    }
+
     function ajax(mothod, url, success) {
         var r = new XMLHttpRequest();
         r.onreadystatechange = function () {
@@ -83,6 +96,6 @@
             success(JSON.parse(json));
         });
     };
-    this.$ = $;
-})([].slice);
+    return $;
+})();
 
--- test/zeptoSpec.js
+++ test/zeptoSpec.js
@@ -152,4 +152,5 @@ describe('zepto suite text', function () {
             });
         });
     });
+
 });
