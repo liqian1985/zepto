99c51e291fbf92d1099554dd0f725454fccb4d29
Li Qian <li_qian008@163.com>
2015-07-01 09:28:31 +0800

    添加手机相关的几个事件：'swipeLeft', 'swipeRight', 'doubleTap', 'tap

--- src/zepto.js
+++ src/zepto.js
@@ -218,6 +218,46 @@ var $ = (function (d) {
         })(ADJ_OPS[k]);
     }
 
+    ['swipeLeft', 'swipeRight', 'doubleTap', 'tap'].forEach(function (m) {
+        $.fn[m] = function (callback) {
+            return this.bind(m, callback);
+        }
+    });
+
+    function dispatch(event, target) {
+        var e = document.createEvent('Events');
+        e.initEvent(event, true, false);
+        target.dispatchEvent(e);
+    }
+
+    d.ontouchstart = function(e) {
+        var now = Date.now(),
+            t = e.touches[0].target,
+            delta = now - (t.last || now);
+        t.x1 = e.touches[0].pageX;
+        if (delta > 0 && delta <= 800) {
+            dispatch('doubleTap', t);
+            t.last = 0;
+        } else {
+            t.last = now;
+        }
+    };
+
+    d.ontouchmove = function(e) {
+        e.touches[0].target.x2 = e.touches[0].pageX;
+    }
+
+    d.ontouchend = function(e) {
+        var t = e.target;
+        if (t.x2 > 0) {
+            t.x1 - t.x2 > 30 && dispatch('swipeLeft', t);
+            t.x1 - t.x2 < -30 && dispatch('swipeRight', t);
+            t.x1 = t.x2 = t.last = 0;
+        } else if (t.last != 0) {
+            dispatch('tap', t);
+        }
+    }
+
     function ajax(mothod, url, success) {
         var r = new XMLHttpRequest();
         r.onreadystatechange = function () {
@@ -226,6 +266,7 @@ var $ = (function (d) {
             };
         };
         r.open(method, url, true);
+        r.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
         r.send(null);
     }
     $.get = function (url, success) {
