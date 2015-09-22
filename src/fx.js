52c94082980d89b8cc85659a2cae4d5913ad4a05
Li Qian <li_qian008@163.com>
2015-09-22 09:45:15 +0800

    整理fx.js

--- src/fx.js
+++ src/fx.js
@@ -1,22 +1,28 @@
 (function($, undefined){
+    var supportedTransforms = [
+        'scale',     'scaleX',     'scaleY',
+        'translate', 'translateX', 'translateY', 'translate3d',
+        'skew',      'skewX',      'skewY',
+        'rotate',    'rotateX',    'rotateY',    'rotateZ',
+        'matrix'
+    ];
+
     $.fn.anim = function(properties, duration, ease, callback) {
         var transforms = [],
-            opacity,
+            cssProperties = {},
             key,
             that = this;
 
         for (key in properties) {
-            if (key === 'opacity') {
-                opacity = properties[key];
-            } else {
+            if (supportedTransforms.indexOf(key) !== -1) {
+
+                // Transform rule
                 transforms.push(key + '(' + properties[key] + ')');
+
+            } else {
+                // CSS property
+                cssProperties[key] = properties[key];
             }
-            var wrappedCallback = function () {
-                that.css( { '-webkit-transition': 'none' } );
-                if ( $.isFunction(callback) ) {
-                    callback();
-                }
-            };
         }
 
         if (parseFloat(duration) !== 0) {
@@ -25,10 +31,11 @@
             setTimeout(wrappedCallback, 0);
         }
 
-        return this.css({
-            '-webkit-transition': 'all ' + (duration !== undefined ? duration : 0.5) + 's ' + (ease || ''),
-            '-webkit-transform': transforms.join(' '),
-            opacity: opacity
-        });
+        return this.css(
+            $.extend({
+                '-webkit-transition': 'all ' + (duration !== undefined ? duration : 0.5) + 's ' + (ease || ''),
+                '-webkit-transform': transforms.join(' ')
+            }, cssProperties)
+        );
     }
 })(Zepto);
